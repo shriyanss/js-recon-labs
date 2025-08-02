67ce1686e25432c20ac056df810351deb7bee61c
Shriyans Sudhi <shriyanss@ss0x00.com>
2025-08-02 13:22:43 +0530

    feat: add About and Contact pages with placeholder content

---
+++ next_js/dom-xss-postMessage-jsUrl/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">About Us</h1>
+      <p>Started before you were born, we are better than you in building CI pipelines. Wanna see a demo? Send us your cloud credentials and we&apos;ll deploy a pipeline like you&apos;ve never seen before.</p>
+    </div>
+  );
+}
---
+++ next_js/dom-xss-postMessage-jsUrl/src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
+      <p>Made up your mind? Drop us an email on contact@127.0.42.55 and we promise that you&apos;ll never see your customer data on dark web.</p>
+    </div>
+  );
+}
